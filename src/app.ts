6c87b5dd7b23df433bdc997dbd5d49f59dfc05cc
mb-tousif <meta.block.tousif@gmail.com>
2023-09-09 23:59:19 +0600

    Soket.io added in pakages

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
+import httpStatus from 'http-status'
 const app: Application = express()
 
 app.use(cors())
@@ -11,6 +12,20 @@ app.use(express.urlencoded({ extended: true }))
 //Testing
 app.get('/', (req: Request, res: Response) => {
   res.send('Working Successfully')
+})
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  })
+  next()
 })
 
 export default app
